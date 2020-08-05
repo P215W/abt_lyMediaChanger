f99170c98201a363644777bd2576ebe44f3d6c94
Marc Grewe <aero322@gmail.com>
2020-08-05 07:07:55 +0200

    first real with readme

--- src/jobs/templateForJob.js
+++ src/jobs/templateForJob.js
@@ -4,4 +4,4 @@
 
 // call modules with above CONST'S as their respective arguments:
     // makeLyMediaList(searchParams1-x); // returns an array of IDs
-    // singleItemRemoveTextFromDescription(changeParams1-x);
+    // singleItemRemoveTextFromDescription(changeParams1-n);
